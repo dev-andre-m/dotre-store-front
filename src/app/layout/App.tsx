d7aa7f7021281a20bda596a5d58c804501cac0de
Andre Mendes <dev.andre.m@icloud.com>
2025-09-15 14:58:01 +0200

    added product model, refactored project structure

--- src/app/layout/App.tsx
+++ src/app/layout/App.tsx
@@ -1,8 +1,9 @@
 import {useEffect, useState} from "react";
+import type {Product} from "../models/product.ts";
 
 function App() {
 
-    const [products, setProducts] = useState<{name: string, price: number}[]>([])
+    const [products, setProducts] = useState<Product[]>([])
 
     useEffect(() => {
         fetch('https://localhost:5001/api/products')
@@ -12,7 +13,17 @@ function App() {
 
     const addProduct = () => {
         setProducts(prevState => [...prevState,
-            {name: 'product' + (prevState.length + 1), price: prevState.length * 100 + 100}])
+            {
+                id: prevState.length + 1,
+                name: 'product' + (prevState.length + 1),
+                price: prevState.length * 100 + 100,
+                quantityInStock: 100,
+                description: 'test',
+                pictureUrl: 'https://picsum.photo/200',
+                type: 'test',
+                brand: 'test'
+            }
+        ])
     }
 
     return (
