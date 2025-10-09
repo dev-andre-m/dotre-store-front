b18aaa5283256128eadc0a0550d83c2c29efe071
Andre Mendes <dev.andre.m@icloud.com>
2025-10-09 14:43:19 +0200

    added fetch product by id

--- src/features/catalog/ProductDetails.tsx
+++ src/features/catalog/ProductDetails.tsx
@@ -1,8 +1,23 @@
 // import React from 'react';
 
+import {useParams} from "react-router-dom";
+import {useEffect, useState} from "react";
+import type {Product} from "../../app/models/product.ts";
+
 function ProductDetails() {
+
+    const {id} = useParams();
+    const [product, setProduct] = useState<Product | null>(null);
+
+    useEffect(() => {
+        fetch(`https://localhost:5001/api/products/${id}`)
+            .then(res => res.json())
+            .then(data => setProduct(data))
+            .catch(err => console.log(err));
+    }, [id])
+
     return (
-        <div>ProductDetails</div>
+        <div>{product?.name}</div>
     );
 }
 
